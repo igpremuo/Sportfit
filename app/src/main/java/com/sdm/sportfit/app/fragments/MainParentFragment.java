c1f8823b3079b23220e5953a75c13337f9759163
igpremuo <igpremuo@inf.upv.es>
2014-03-31 22:48:37 +0200

    First commit

---
+++ app/src/main/java/com/sdm/sportfit/app/fragments/MainParentFragment.java
@@ -0,0 +1,28 @@
+package com.sdm.sportfit.app.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.sdm.sportfit.app.MainActivity;
+import com.sdm.sportfit.app.R;
+
+/**
+ * Created by nacho on 31/03/14.
+ */
+public class MainParentFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("Main Fragment");
+        return rootView;
+    }
+
+}
