8eed3a7dbc1f911ebf44e23a32561f26508747c1
joscerod <joscerod@gmail.com>
2014-04-18 15:20:01 +0200

    creacion de preferen para guardar datos de tipo de training y avisos.

--- app/src/main/java/com/sdm/sportfit/app/fragments/TrainTrainingFragment.java
+++ app/src/main/java/com/sdm/sportfit/app/fragments/TrainTrainingFragment.java
@@ -1,5 +1,6 @@
 package com.sdm.sportfit.app.fragments;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,16 +8,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
 import com.sdm.sportfit.app.MainActivity;
 import com.sdm.sportfit.app.R;
+import com.sdm.sportfit.app.persistence.misSharedPreferences;
 
 /**
  * Created by nacho on 1/04/14.
  */
 public class TrainTrainingFragment extends Fragment {
 
+    public static final String ANDAR = "andar";
+    public static final String CORRER = "correr";
+    public static final String BICI = "bici";
+    public static final String DISTANCIA = "distancia";
+    public static final String TIEMPO = "tiempo";
+
+
     MainActivity mMainActivity;
+    //Views
+    Button mButton;
+    RadioGroup mRadioGroupDeporte;
+    RadioGroup mRadioGroupAviso;
+
+    String mDeporte;
+    String mAviso;
 
     @Override
     public void onAttach(Activity activity) {
@@ -29,14 +46,72 @@ public class TrainTrainingFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_train_training, container, false);
 
-        Button button = (Button) rootView.findViewById(R.id.train_training_button);
-        button.setOnClickListener(new View.OnClickListener() {
+        mButton = (Button) rootView.findViewById(R.id.train_training_button);
+        mRadioGroupDeporte =(RadioGroup)rootView.findViewById(R.id.train_training_rg_tipo_deporte);
+        mRadioGroupAviso =(RadioGroup)rootView.findViewById(R.id.train_training_rg_tipo_aviso);
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        restaurarDatos();
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mMainActivity.openFragmentAtPos(0,0);
+                int deporte = mRadioGroupDeporte.getCheckedRadioButtonId();
+                switch (deporte) {
+                    case R.id.train_training_rb_andar:
+                        mDeporte = ANDAR;
+                        break;
+                    case R.id.train_training_rb_correr:
+                        mDeporte=CORRER;
+                        break;
+                    case R.id.train_training_rb_bicicleta:
+                        mDeporte=BICI;
+                        break;
+                    default:
+                        mDeporte=CORRER;
+                        break;
+                }
+                int aviso =mRadioGroupAviso.getCheckedRadioButtonId();
+                switch (aviso){
+                    case R.id.train_training_rb_aviso_distancia:
+                        mAviso = DISTANCIA;
+                        break;
+                    case R.id.train_training_rb_aviso_tiempo:
+                        mAviso = TIEMPO;
+                        break;
+                    default:
+                        mAviso=DISTANCIA;
+                        break;
+                }
+
+                //Guarda configuracion de entreanmiento
+                misSharedPreferences.guardarConfiguracionDeporte(getActivity(),mDeporte,mAviso);
+                mMainActivity.openFragmentAtPos(0, 0);
             }
         });
 
-        return rootView;
+    }
+    private void restaurarDatos(){
+        Bundle datos = misSharedPreferences.restaurarConfiguracionDeporte(getActivity());
+        mDeporte = datos.getString(misSharedPreferences.TIPODEPORTE);
+        mAviso =datos.getString(misSharedPreferences.TIPOAVISO);
+
+        if(mDeporte.equals(ANDAR)){
+            mRadioGroupDeporte.check(R.id.train_training_rb_andar);
+        }
+        else if(mDeporte.equals(CORRER)){
+            mRadioGroupDeporte.check(R.id.train_training_rb_correr);
+        }
+        else  mRadioGroupDeporte.check(R.id.train_training_rb_bicicleta);
+
+        if(mAviso.equals(DISTANCIA)){
+            mRadioGroupAviso.check(R.id.train_training_rb_aviso_distancia);
+        }
+        else mRadioGroupAviso.check(R.id.train_training_rb_aviso_tiempo);
     }
 }
